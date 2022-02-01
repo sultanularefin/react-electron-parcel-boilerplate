50d461d5a2c059fd78f469489a0344b58177e51d
sultanularefin <mhmdarefin@gmail.com>
2022-02-01 18:39:54 +0600

    problem in ---src/electron/dev.cjs

---
+++ src/electron/dev.cjs
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron');
+
+try {
+    require('electron-reloader')(module);
+} catch (_) {}
+
+const createWindow = () => {
+    const window = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    // window.loadFile('build/index.html');
+    window.loadFile('dist/index.html');
+};
+
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => app.quit());
